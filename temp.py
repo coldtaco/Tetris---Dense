cb727d795a09b1f1ad3a4be892e1978a097a06f8
coldtaco <coldtaco1@hotmail.comgit config --global user.email yuhang124@hotmail.com>
2019-10-08 15:23:59 +1100

    test

--- temp.py
+++ temp.py
@@ -1,56 +1,13 @@
-if piece == 0:
-            if self.rotation == 0:
-                return ((x-1,y),(x,y),(x+1,y),(x+2,y))
-            elif self.rotation == 1:
-                return ((x,y-1),(x,y),(x,y+1),(x,y)+2)
-            elif self.rotation == 2:
-                return ((x-2,y),(x-1,y),(x,y),(x+1,y))
-            elif self.rotation == 3:
-                return ((x,y-2),(x,y-1),(x,y),(x,y+1))
-        elif piece == 1:
-            return ((x,y),(x,y+1),(x+1,y),(x+1,y+1))
-        elif piece == 2:
-            if self.rotation == 0:
-                return ((x-1,y),(x,y),(x+1,y),(x,y+1))
-            elif self.rotation == 1:
-                return ((x,y+1),(x,y),(x,y-1),(x-1,y))
-            elif self.rotation == 2:
-                return ((x-1,y),(x,y),(x+1,y),(x,y-1))
-            elif self.rotation == 3:
-                return ((x,y+1),(x,y),(x,y-1),(x+1,y))
-        elif piece == 3:
-            if self.rotation == 0:
-                return ((x-1,y),(x,y),(x+1,y),(x-1,y-1))
-            elif self.rotation == 1:
-                return ((x+1,y-1),(x,y+1),(x,y),(x,y-1))
-            elif self.rotation == 2:
-                return ((x-1,y),(x,y),(x+1,y),(x+1,y+1))
-            elif self.rotation == 3:
-                return ((x-1,y+1),(x,y+1),(x,y),(x,y-1))
-        elif piece == 4:
-            if self.rotation == 0:
-                return ((x-1,y),(x,y),(x+1,y),(x+1,y-1))
-            elif self.rotation == 1:
-                return ((x+1,y+1),(x,y+1),(x,y),(x,y-1))
-            elif self.rotation == 2:
-                return ((x-1,y),(x,y),(x+1,y),(x-1,y+1))
-            elif self.rotation == 3:
-                return ((x-1,y-1),(x,y+1),(x,y),(x,y-1))
-        elif piece == 5:
-            if self.rotation == 0:
-                return ((x,y),(x+1,y),(x,y-1),(x-1,y+1))
-            elif self.rotation == 1:
-                return ((x-1,y-1),(x-1,y),(x,y),(x,y+1))
-            elif self.rotation == 2:
-                return ((x-1,y),(x,y-1),(x,y),(x+1,y-1))
-            elif self.rotation == 3:
-                return ((x,y+1),(x,y),(x+1,y),(x+1,y+1))
-        elif piece == 6:
-            if self.rotation == 0:
-                return ((x-1,y),(x,y),(x,y-1),(x+1,y+1))
-            elif self.rotation == 1:
-                return ((x,y-1),(x,y),(x-1,y),(x-1,y+1))
-            elif self.rotation == 2:
-                return ((x-1,y-1),(x,y-1),(x,y),(x+1,y))
-            elif self.rotation == 3:
-                return ((x,y+1),(x,y),(x+1,y),(x+1,y-1))
+from tetris import Game
+import scoring
+import numpy as np
+
+x = Game()
+board = x.board
+for y in range(len(board)):
+    if y > 16:
+        board[y][0] = 2
+        board[y][2] = 2
+        board[y][3] = 2
+print(board)
+print(scoring.distance(board))
