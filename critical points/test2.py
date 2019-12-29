5a8d2b9816076a33d9f5e624da9a7ca9279c461e
coldtaco <36543008+coldtaco@users.noreply.github.com>
2019-12-29 16:26:31 +1100

    Update test2.py

--- critical points/test2.py
+++ critical points/test2.py
@@ -7,3 +7,10 @@ class Point():
         self.args = args
         self.result = result
 
+def newVal(x1,x2,y1,y2, iterations, gradientMagnitude = 0.5):
+    assert(x2 != x1)
+    m = (y2-y1)/(x2-x1)
+    if m == 0:
+        return (x2+x1)/2
+    adjustment = math.atan(m/(iterations/10))*gradientMagnitude*(abs(x2-x1)**(1/3))
+    return x1 - adjustment if y1 < y2 else x2 - adjustment
